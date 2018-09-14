0bb7ef9b100ecb38bf3fd61457ae3d946641b994
18475573686 <39574204+18475573686@users.noreply.github.com>
2018-09-14 11:48:07 +0800

    连接MongoDB数据库，商品列表查询接口

---
+++ server/routes/goods.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('./../models/goods.js')
+
+// 连接MongoDB数据库
+mongoose.connect('mongodb://127.0.0.1:27017/vue_shop');
+
+mongoose.connection.on('connected', function() { // 数据库连接事件的监听
+  console.log('MongoDB connected success.')
+});
+
+mongoose.connection.on('error', function() { // 数据库错误事件的监听
+  console.log('MongoDB connected fail.')
+});
+
+mongoose.connection.on('disconnected', function() { // 数据库断开事件的监听
+  console.log('MongoDB connected disconnected.')
+});
+
+/* 查询商品列表的数据 */
+router.get('/', function(req, res, next) {
+  Goods.find({}, function(err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      });
+    } else {
+      res.json({
+        status: '0',
+        msg: '',
+        result: {
+          count: doc.length,
+          list: doc
+        }
+      });
+    }
+  })
+});
+
+module.exports = router;
--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('用户路由'); // 返回一个字符串
-});
-
-module.exports = router;
